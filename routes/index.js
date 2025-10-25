2e8a578a3f3db51e04f06d7afc6d398ae2757191
JeremyRhain <schmidtjeremyr@gmail.com>
2025-10-24 18:48:54 -0700

    commit 24-pc

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
 // routes/index.js
 const router = require("express").Router();
 const { NOT_FOUND_ERROR_CODE } = require("../utils/errors");
-
 const { login, createUser } = require("../controllers/users");
 const { getClothingItems } = require("../controllers/clothingItems");
 const auth = require("../middlewares/auth");
 
-router.post("/signin", login);
-router.post("/signup", createUser);
-router.get("/items", auth, getClothingItems);
-
-router.use(auth); // Protect all routes below this middleware
-
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
+
+router.post("/signin", login);
+router.post("/signup", createUser);
+router.get("/items", getClothingItems);
+
+router.use(auth); // Protect all routes below this middleware
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemsRouter);
