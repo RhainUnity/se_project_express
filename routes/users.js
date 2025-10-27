a0eb96b3ddf7b2fa090cff6566c635b5bc853fbb
JeremyRhain <schmidtjeremyr@gmail.com>
2025-10-26 23:11:16 -0700

    commit-31-pc

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { getUsers, createUser, updateUser } = require("../controllers/users");
+const { createUser, updateUser } = require("../controllers/users");
 
 router.post("/", createUser);
 
