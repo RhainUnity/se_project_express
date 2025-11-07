9450c1cfb94385582ab105cde65f827e5f6fe8ed
JeremyRhain <schmidtjeremyr@gmail.com>
2025-11-06 19:07:44 -0800

    commit-fullstack-1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,14 @@ const { JWT_SECRET } = require("../utils/config");
 const { UNAUTHORIZED_ERROR_CODE } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
+  // shortcut to get token from header for DEV purposes!!! //
+  // /////////
+  if (process.env.NODE_ENV === "development") {
+    // Skip auth entirely in dev
+    req.user = { _id: "6729aa7d39d7e0f1dc123456" }; // <- use a valid ObjectId from your DB if possible
+    return next();
+  }
+
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
