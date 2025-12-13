11c804cc7e9a364a34dd94818626455382e13a7b
RhainUnity <schmidtjeremyr@gmail.com>
2025-12-13 14:18:54 -0800

    Allow CORS from production frontend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // app.js
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,30 +10,50 @@ const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
-const { PORT, MONGO_URL, NODE_ENV } = process.env;
+const { PORT, MONGO_URL } = process.env;
 
+// --- DB ---
 mongoose
   .connect(MONGO_URL)
   .then(() => console.log("Connected to MongoDB")) // eslint-disable-line no-console
   .catch((err) => console.error("Could not connect to MongoDB", err)); // eslint-disable-line no-console
 
+// --- CORS ---
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://jrsttwtwr2025.twilightparadox.com",
+  "https://www.jrsttwtwr2025.twilightparadox.com",
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // adding frontend origin
-    credentials: false, // (true if I switch to cookies later)
+    origin(origin, callback) {
+      // allow non-browser clients (no Origin) + our allowed origins
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: false, // set to true if you ever switch to cookies
   })
 );
+
 app.use(express.json());
 
+// log requests *before* routes (optional but nice)
+app.use(requestLogger);
+
+// routes
 app.use("/", mainRouter);
 
-app.use(requestLogger);
-app.use(routes);
+// log errors
 app.use(errorLogger);
 
+// celebrate errors
 app.use(errors());
-app.use(errorHandler);
 
+// custom error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`); // eslint-disable-line no-console
